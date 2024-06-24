e59a40c832bce58092edefb5242474029f888d4e
Axonide <alvusebastian@gmail.com>
2024-06-24 16:02:34 +0700

    06242024

--- src/MainLandingPage.js
+++ src/MainLandingPage.js
@@ -40,37 +40,36 @@ export default () => {
         subheading={<Subheading>Established Since 2014</Subheading>}
         heading={
           <>
-            We've been serving for
+            Serving Flavorful Fried Chicken
             <wbr /> <HighlightedText>over 5 years.</HighlightedText>
           </>
         }
         description={
           <Description>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua.
+            Crispy, golden-brown chicken<br />has delighted taste buds since day one.
             <br />
             <br />
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Come experience the deliciousness that stands the test of time.
           </Description>
         }
         buttonRounded={false}
         textOnLeft={false}
         primaryButtonText="Latest Offers"
         imageSrc={
-          "https://images.unsplash.com/photo-1460306855393-0410f61241c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=768&q=80"
+          "https://www.shutterstock.com/shutterstock/photos/2234906561/display_1500/stock-photo-woman-hand-taking-the-fried-chicken-wings-by-hands-over-dark-background-with-copy-space-2234906561.jpg"
         }
         imageCss={imageCss}
         imageDecoratorBlob={true}
         imageDecoratorBlobCss={tw`left-1/2 -translate-x-1/2 md:w-32 md:h-32 opacity-25`}
       />
       {/* TabGrid Component also accepts a tabs prop to customize the tabs and its content directly. Please open the TabGrid component file to see the structure of the tabs props.*/}
-      <TabGrid
+      {/* <TabGrid
         heading={
           <>
             Checkout our <HighlightedText>menu.</HighlightedText>
           </>
         }
-      />
+      /> */}
       <Features
         heading={
           <>
@@ -81,19 +80,19 @@ export default () => {
           {
             imageSrc: shopIconImageSrc,
             title: "230+ Locations",
-            description: "Lorem ipsum donor amet siti ceali placeholder text",
+            description: "Preaching the happiness of jack's signature fried chicken",
             url: "https://google.com"
           },
           {
             imageSrc: chefIconImageSrc,
-            title: "Professional Chefs",
-            description: "Lorem ipsum donor amet siti ceali placeholder text",
+            title: "Constant Crisp",
+            description: "Quality comes first to deliver the crispiness and savoriness of Jack's",
             url: "https://timerse.com"
           },
           {
             imageSrc: celebrationIconImageSrc,
             title: "Birthday Catering",
-            description: "Lorem ipsum donor amet siti ceali placeholder text",
+            description: "Celebrate your +1 happiness with Jack's",
             url: "https://reddit.com"
           }
         ]}
